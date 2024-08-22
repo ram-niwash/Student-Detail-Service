b0e348db9eb1506f2cdb1dafc3193771447c88df
Ram Niwash <rniwash122@gmail.com>
2024-08-22 21:45:22 +0530

    Project Created

---
+++ src/main/java/com/myapp/sds/StudentDetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.sds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailServiceApplication.class, args);
+	}
+
+}
